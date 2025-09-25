6a1122f67f9a273f8de4684cdf2954ffa50992d5
Max <olejnikmax@gmail.com>
2025-09-25 12:54:08 +0300

    parsing and io

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		fmt.Fprintln(os.Stderr, "Error:", e)
+		fatalUsage()
+	}
+}
+
+func fatalUsage() {
+	fmt.Println("Usage: ./md-to-html file.md")
+	os.Exit(1)
+}
 
 func main() {
-	fmt.Println("Hello app")
+
+	if len(os.Args) != 2 || !strings.HasSuffix(os.Args[1], ".md") {
+		fatalUsage()
+	}
+
+	dat, err := os.ReadFile(os.Args[1])
+	check(err)
+
+	fmt.Printf("File len:%d", len(dat))
 }
